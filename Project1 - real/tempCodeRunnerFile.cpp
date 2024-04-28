cb00671266d17d1ecc06aab97e6f64e391db0120
Franklin <121955499+FagbemiAdemolaFranklin@users.noreply.github.com>
2024-04-28 01:19:54 +0100

    New features.
    New features such as saving uncompleted game in a text file, loading the stats of a previous player, computing the statictics of the top 10 players and so on were implented. However, there are still some bugs to fix on those features.

---
+++ Project1 - real/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+for (int i = 0; i < 9; ++i) {
+                    //     symbol_find = current_line.find("difficulty: ");
+                    //     game_array[i] = current_line.at(symbol_find+i);
+                    // }
